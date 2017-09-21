541627fcc089e88441a87dca6b26bb8c11bcf3bf
omadoyeabraham <omadoyeabraham@gmail.com>
2017-09-21 20:56:20 +0100

    deleted adminLTE styles

---
+++ test/unit/specs/Login.spec.js
@@ -0,0 +1,19 @@
+/**
+ * Test suite to test the Login Component
+ *
+ * @author Omadoye Abraham
+ */
+
+ // import Vue from 'vue'
+ // import Login from '@components/Login'
+
+ describe('Login.vue', () => {
+   it('should render the username, password and submit components', () => {
+     // const Constructor = Vue.extend(Login)
+     // const vm = new Constructor().$mount()
+    //  expect(vm.$el.querySelector('.hello h1').textContent)
+    //   .to.equal('YHHHHHHHHHHHH')
+     expect(1)
+        .to.equal(1)
+   })
+ })
